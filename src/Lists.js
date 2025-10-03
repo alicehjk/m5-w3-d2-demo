f8c8cba65d0c7626e6f4ab75a73fe38d37d9d7cc
Alice Hyejin Kim <hyejik5@uci.edu>
2025-10-02 19:12:51 -0700

    update

--- src/Lists.js
+++ src/Lists.js
@@ -1,31 +1,45 @@
-import React from 'react'
+import React from "react";
+import UpdateList from "./UpdateList";
+import DeleteList from "./DeleteList";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Lists(props) {
-    let listrows =[];
-    props.alldata.forEach(element => {
-        listrows.push(
-            <tr key={element.id}>
-                <td>{element.id}</td>
-                <td>{element.title}</td>
-                <td>{element.author}</td>
-            </tr>
-        )
-    })
-    return (
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Update</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>{listrows}</tbody>
-        </table>
-    )
+  let listrows = [];
+  props.alldata.forEach((element) => {
+    listrows.push(
+      <tr key={element.id}>
+        <td>{element.title}</td>
+        <td>{element.author}</td>
+        <td>
+          <UpdateList
+            elementId={element.id}
+            singledata={props.singledata}
+            getList={props.getList}
+            updateList={props.updateList}
+            handleChange={props.handleChange}
+          ></UpdateList>
+          <DeleteList
+            elementId={element.id}
+            singledata={props.singledata}
+            getList={props.getList}
+            deleteList={props.deleteList}
+          ></DeleteList>
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>{listrows}</tbody>
+    </table>
+  );
 }
 
 export default Lists;
